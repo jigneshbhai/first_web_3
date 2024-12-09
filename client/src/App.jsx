4f00f6ce682a42a975b194bacd553c6229853ff1
jignesh <85551495+jigneshbhai@users.noreply.github.com>
2024-12-09 10:55:50 +0530

    Navbar and Welcome component done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Navbar, Welcome,Footer, Services, Transactions } from './components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
   )
 }
 
